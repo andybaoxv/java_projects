5d21165045b6f0fcf35648095edbd77da2634898
andybaoxv <changyale123@gmail.com>
2016-02-18 12:24:15 -0500

    add summary on parameter pass

---
+++ ParameterPass/Python_exp_2.py
@@ -0,0 +1,10 @@
+def try_to_change_list_reference(the_list):
+    print 'got', the_list
+    the_list = ['and', 'we', 'can', 'not', 'lie']
+    print 'set to', the_list
+
+outer_list = ['we', 'like', 'proper', 'English']
+
+print 'before, outer_list =', outer_list
+try_to_change_list_reference(outer_list)
+print 'after, outer_list =', outer_list
